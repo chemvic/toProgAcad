8c58b531bc0f051eec364fc63ddcf9c17230b094
chemvic <chemvic79@gmail.com>
2022-09-14 13:46:09 +0300

    прописал адрес скрипта

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 let bitcoinPrice=0;
 let dollarsValue=0;
 let canBuy=0;
-bitcoinPrice=prompt("What is Bitcoin price today?", [0]);
-dollarsValue=prompt("How much $ do you have?", [0]);
+bitcoinPrice=Number(prompt("What is Bitcoin price today?", [0]));
+dollarsValue=Number(prompt("How much $ do you have?", [0]));
 console.log(`You can buy ${dollarsValue/bitcoinPrice} BTC`)
