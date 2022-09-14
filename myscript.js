cd89fef847eb89f5c77d08127c426f329ac91a32
chemvic <chemvic79@gmail.com>
2022-09-14 14:24:59 +0300

    вывод на экран через document.write

--- myscript.js
+++ myscript.js
@@ -3,4 +3,5 @@ let dollarsValue=0;
 let canBuy=0;
 bitcoinPrice=Number(prompt("What is Bitcoin price today?", [0]));
 dollarsValue=Number(prompt("How much $ do you have?", [0]));
-console.log(`You can buy ${dollarsValue/bitcoinPrice} BTC`)
+canBuy=`You can buy ${dollarsValue/bitcoinPrice} BTC`;
+document.write(canBuy);
