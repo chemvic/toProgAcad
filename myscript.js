f0a8b6e2ab4936f16b60ed4c13f74d6ecbf16f79
chemvic <chemvic79@gmail.com>
2022-09-14 13:26:01 +0300

    чуть поменял))

--- myscript.js
+++ myscript.js
@@ -3,4 +3,4 @@ let dollarsValue=0;
 let canBuy=0;
 bitcoinPrice=prompt("What is Bitcoin price today?", [0]);
 dollarsValue=prompt("How much $ do you have?", [0]);
-console.log("You can buy {dollarsValue/bitcoinPrice} BTC")
+console.log(`You can buy ${dollarsValue/bitcoinPrice} BTC`)
